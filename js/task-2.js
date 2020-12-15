74db26f0706f063be02571b069935cecd7c75ffb
Iryna Yasynska <irina159159@gmail.com>
2020-12-15 05:50:09 +0200

    промис

---
+++ js/task-2.js
@@ -0,0 +1,80 @@
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+
+const toggleUserState = (allUsers, userName) => {
+        const updatedUsers = allUsers.map(user =>
+        user.name === userName ? { ...user, active: !user.active } : user,
+  );
+  
+  return Promise.resolve(updatedUsers);
+};
+
+const logger = updatedUsers => console.table(updatedUsers);
+
+/*
+ * Сейчас работает так
+ */
+// toggleUserState(users, 'Mango', logger);
+// toggleUserState(users, 'Lux', logger);
+
+/*
+ * Должно работать так
+ */
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // return new Promise((resolve, reject) => {
+  //   if (!allUsers) {
+  //     reject(Error('Userlist is invalid'))
+  //   };      // resolve(updatedUsers)
